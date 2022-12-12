3ebb6d377577dc103b9448fcb14473dd99acc812
guliev <guliev@internet.ru>
2022-12-12 18:02:04 +0200

    Use vite js, and create minimal header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './style.module.css';
+
+export const Header: React.FC = (): JSX.Element => {
+    return <div className={styles.header}>
+        <h1>Вконтакте</h1>
+        <input type="text" className={styles.search} placeholder='Поиск' />
+    </div>
+};

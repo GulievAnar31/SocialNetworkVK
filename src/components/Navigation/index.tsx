f33f8467d68eb1f258586973f0cb01f7ac61a997
guliev <guliev@internet.ru>
2022-12-18 12:02:13 +0200

    fix adaptive, add userInfo, add miniMenuBtn

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import styles from './style.module.scss';
 
 export const Navigation = () => {
-    return <nav className={styles.navigation}>
+    const [activeMiniMenu, setActiveMiniMenu] = React.useState(false);
+
+    const MiniMenu = () => {
+        return <div>
+            <button onClick={() => setActiveMiniMenu(!activeMiniMenu)} className={styles.navigationMiniActive}>{'>'}</button>
+            {activeMiniMenu && <nav className={styles.navigationMini}>
+                <a href="#">Моя страница</a>
+                <a href="#">Новости</a>
+                <a href="#">Друзья</a>
+                <a href="#">Настройки</a>
+            </nav>}
+        </div>
+    }
+
+    return<>
+    <MiniMenu />
+    <nav className={styles.navigation}>
         <a href="#">Моя страница</a>
         <a href="#">Новости</a>
         <a href="#">Друзья</a>
         <a href="#">Настройки</a>
     </nav>
+    </>
 };
--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -3,6 +3,14 @@ import styles from './style.module.scss';
 
 export const UserInfo = () => {
     return <div className={styles.userInfo}>
-        <img src="https://sun9-26.userapi.com/impg/x5NfpyqCwc0klKjLLGCYcPM96hQnj09zzi0rng/lZ92WITuEvg.jpg?size=200x267&quality=96&sign=1a7d7f36e4be0622551f5ce269c5237c&c_uniq_tag=FRDzsVoSi-2PU34itAzooaT4-1kBe2lrQZn-L_N5ll0&type=album" alt="me" />
+        <div>
+        <img src="https://sun9-26.userapi.com/impg/x5NfpyqCwc0klKjLLGCYcPM96hQnj09zzi0rng/lZ92WITuEvg.jpg?size=200x267&quality=96&sign=1a7d7f36e4be0622551f5ce269c5237c&c_uniq_tag=FRDzsVoSi-2PU34itAzooaT4-1kBe2lrQZn-L_N5ll0&type=album" alt="me" />    
+        </div>
+
+        <div className={styles.info}>
+            <span>Anar Guliev</span>
+            <span>Санкт-Петербург</span>
+            <span>31.01.1998</span>
+        </div>
     </div>
 };
